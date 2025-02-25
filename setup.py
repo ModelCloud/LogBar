86ccb71442e9cb56994932a7fe9a6621f2559966
CSY-ModelCloud <csy@modelcloud.ai>
2025-02-25 17:32:13 +0800

    bump version to v0.0.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
-__version__ = "0.0.1-dev"
+__version__ = "0.0.2"
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
