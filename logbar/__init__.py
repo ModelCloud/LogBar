97929e5e8c2b1510d172f8eed5dd0e24dee5d34a
Qubitium <qubitium@modelcloud.ai>
2025-10-05 09:48:43 +0000

    lic
    Signed-off-by: Qubitium <qubitium@modelcloud.ai>

--- logbar/__init__.py
+++ logbar/__init__.py
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: 2024-2025 ModelCloud.ai
+# SPDX-FileCopyrightText: 2024-2025 qubitium@modelcloud.ai
+# SPDX-License-Identifier: Apache-2.0
+# Contact: qubitium@modelcloud.ai, x.com/qubitium
+
 """Top-level package for the LogBar utility."""
 
 from .logbar import LogBar
