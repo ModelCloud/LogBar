a9feb141b7a33188fa74c17840b0372174d60b62
CSY-ModelCloud <csy@modelcloud.ai>
2025-12-12 10:31:55 +0800

    [CI ] fix CI for unit test (#29)
    * update unit test CI env
    
    * remove dependencies
    
    * add pyenv
    
    * create venv with matrix
    
    * fix no test

--- tests/test_example.py
+++ tests/test_example.py
@@ -4,20 +4,24 @@
 # Contact: qubitium@modelcloud.ai, x.com/qubitium
 
 import time
+import unittest
 from logbar import LogBar
 
-log = LogBar.shared()
 
-pb_fetch = log.pb(range(80)).title("Fetch").manual()
-pb_train = log.pb(range(120)).title("Train").manual()
+class TestExample(unittest.TestCase):
+    def test_example(self):
+        log = LogBar.shared()
 
-for _ in pb_fetch:
-    pb_fetch.draw()
-    time.sleep(0.01)
+        pb_fetch = log.pb(range(80)).title("Fetch").manual()
+        pb_train = log.pb(range(120)).title("Train").manual()
 
-for _ in pb_train:
-    pb_train.draw()
-    time.sleep(0.01)
+        for _ in pb_fetch:
+            pb_fetch.draw()
+            time.sleep(0.01)
 
-pb_train.close()
-pb_fetch.close()
+        for _ in pb_train:
+            pb_train.draw()
+            time.sleep(0.01)
+
+        pb_train.close()
+        pb_fetch.close()
